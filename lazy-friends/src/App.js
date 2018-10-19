6b92b36746cf802c51e550d710893d00b3beb293
Jesse McReady <jessemcready@gmail.com>
2018-10-19 09:27:48 -0400

    set up group component files

--- lazy-friends/src/App.js
+++ lazy-friends/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import MainPage from './components/MainPage'
 import './App.css';
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <NavBar />
-          <Route exact path="/" render={ () => <h1>Home</h1> } />
+          <Route exact path="/" component={MainPage} />
           <Route exact path='/profile' render={ () => <h1>Profile</h1>} />
           <Route exact path='/newgroup' render={ () => <h1>New Group</h1>} />
           <Route exact path='/users' render={ () => <h1>Users</h1>} />
---
+++ lazy-friends/src/components/groups/GroupContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GroupContainer extends Component {
+
+  render() {
+    return (
+      <h1>Group Container</h1>
+    );
+  }
+
+}
+
+export default GroupContainer;
