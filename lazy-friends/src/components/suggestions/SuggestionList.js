d865d62b8d33c66867278ffe270d72c1a58676b4
Jesse McReady <jessemcready@gmail.com>
2019-01-17 17:20:44 -0500

    removed comments

--- lazy-friends/src/components/suggestions/SuggestionList.js
+++ lazy-friends/src/components/suggestions/SuggestionList.js
@@ -1,59 +1,9 @@
 import React from 'react';
 import SuggestionItem from './SuggestionItem';
 import { connect } from 'react-redux';
-// import { testSuggs } from '../../constants/suggestions'
 
 class SuggestionList extends React.Component {
-  // state = { suggestions: [] }
-
-  // checkResults = (results) => {
-  //   let isEqual = true
-
-  //   if(this.state.suggestions.length === 0){
-  //     return false
-  //   }
-
-  //   results.forEach( (result, index) => {
-  //     if(result.id !== this.state.suggestions[index].id){
-  //       isEqual = false
-  //     }
-  //   })
-  //   return isEqual
-  // }
-
-  // getSuggestions = () => {
-  //   let results
-
-  //   if(this.props.currentCoords !== ''){
-  //     return fetch('http://localhost:3000/places', {
-  //         method: 'POST',
-  //         headers: {
-  //           'Accept': 'application/json',
-  //           'Content-Type': 'application/json'
-  //         },
-  //         body: JSON.stringify({
-  //           coordinates: this.props.currentCoords,
-  //           preferences: this.props.currentGroup.suggestions
-  //         })
-  //       })
-  //     .then(response => response.json())
-  //     .then(yelpData => {
-  //       if(!!yelpData.businesses){
-  //         if(!(this.checkResults(yelpData.businesses))){
-  //           this.setState({
-  //             suggestions: yelpData.businesses
-  //           })
-  //         }
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //     })
-  //   }
-  // }
-
   render(){
-    // this.getSuggestions()
     if(!this.props.suggestions.businesses){
       return <p>There are no suggestions in this area.</p>
     }
